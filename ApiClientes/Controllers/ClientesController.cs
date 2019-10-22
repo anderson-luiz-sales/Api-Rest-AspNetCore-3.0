2f83abe1541c5f77d1210110f15e2f719262260c
anderson-luiz-sales <anderson.luiz.sales@gmail.com>
2019-10-22 16:56:18 -0300

    Completion of the api rest

--- ApiClientes/Controllers/ClientesController.cs
+++ ApiClientes/Controllers/ClientesController.cs
@@ -34,5 +34,49 @@ namespace ApiClientes.Controllers
 
             return new ObjectResult(cliente);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Cliente cliente)
+        {
+            if (cliente == null)
+                return BadRequest();
+
+            _clienteRepository.Add(cliente);
+
+            return CreatedAtRoute("GetCliente", new  { id = cliente.Id_Cli }, cliente);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Cliente cliente)
+        {
+            if (cliente == null || cliente.Id_Cli != id)
+                return BadRequest();
+
+            var _cliente = _clienteRepository.Find(id);
+
+            if (_cliente == null)
+                return NotFound();
+
+            _cliente.Nome = cliente.Nome;
+            _cliente.Cidade = cliente.Cidade;
+            _cliente.Telefone = cliente.Telefone;
+            _cliente.Celular = cliente.Celular;
+            _cliente.Email = cliente.Email;
+
+            _clienteRepository.Update(_cliente);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var cliente = _clienteRepository.Find(id);
+
+            if (cliente == null)
+                return NotFound();
+
+            _clienteRepository.Remove(id);
+            return new NoContentResult();
+        }
     }
 }
